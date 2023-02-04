59aae2dd0779377c55a0ef8aca976c67803d591c
Terry Grimoire <terry.grimoire@gmail.com>
2023-02-04 19:31:54 +0400

    ok for pizzas

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-import Tarifs from "./pages/Tarifs";
-import Contact from "./pages/Contact";
 import Mentions from "./pages/Mentions";
 
 import "./App.css";
@@ -21,8 +19,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home helmet={helmet} />} />
         <Route path="/Services" element={<Services helmet={helmet} />} />
-        <Route path="/Tarifs" element={<Tarifs helmet={helmet} />} />
-        <Route path="/Contact" element={<Contact helmet={helmet} />} />
         <Route path="/Mentions" element={<Mentions />} />
       </Routes>
       <Footer />
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,17 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import papa from "papaparse";
 
 function Services({ helmet }) {
+  const [pizzas, setPizzas] = useState([]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  const prepareData = (data) => {
+    // j correspond aux lignes de A à ZZZ sur fichier Excel
+    // index
+    // line correspond à
+    // index correspond à
+    // key correspond à
+
+    let obj = {};
+    const json = data.map((line, index) => {
+      if (index > 1) {
+        data[9].forEach((key, j) => {
+          if (line[j] !== "" && line[j] !== "PIZZAS") {
+            obj = { ...obj, [key]: line[j] };
+          }
+        });
+      }
+      return obj;
+    });
+
+    json.shift();
+    setPizzas([...new Set(json)]);
+  };
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_DATA)
+      .then((result) => result.text())
+      .then((text) => papa.parse(text))
+      .then((data) => prepareData(data.data));
+  }, []);
   return (
-    <div>
+    <div className="menu">
       <Helmet>
         <title> {helmet.title} | Services </title>
         <link rel="canonical" href={`${helmet.href}/Services`} />
         <meta name="description" content={helmet.description} />
       </Helmet>
+      <section>
+        <h1>Nos pizzas</h1>
+        <div>
+          {pizzas &&
+            pizzas
+              .filter((el) => el.Nom !== "Nom" && el.Base === "nouveautes")
+              .map((pizza) => (
+                <div className="pizza">
+                  <h3>{pizza.Nom}</h3>
+                  <p>{pizza.Description}</p>
+                  <div>
+                    <ul>
+                      <li>Pâte italienne</li>
+                      <li>26cm : {pizza.p1} €</li>
+                      <li>33cm : {pizza.p2} €</li>
+                    </ul>
+                    <ul>
+                      <li>Pâte fine</li>
+                      <li>33cm : {pizza.p3} €</li>
+                      <li>40cm : {pizza.p4} €</li>
+                    </ul>
+                  </div>
+                </div>
+              ))}
+        </div>
+      </section>
     </div>
   );
 }
