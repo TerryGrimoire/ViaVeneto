d03502ee55f3118228293316a104a51cb11db72f
Terry Grimoire <terry.grimoire@gmail.com>
2023-02-05 13:32:16 +0400

    entions legales ok

--- src/pages/Mentions.jsx
+++ src/pages/Mentions.jsx
@@ -6,15 +6,15 @@ function Mentions() {
   }, []);
 
   const information = {
-    siteLink: " https://lemontecito.fr ",
-    companyName: " Le Montecito ",
-    ownerName: " Simon Giraudet ",
-    email: " lemontecito1011@gmail.com ",
-    phone: " 09 86 50 12 07 ",
-    address: " 6 All. Duquesne, 44000, Nantes.",
-    hostName: " 1and1 ",
-    hostAddress: " Test des addresses 44300 Host",
-    hostPhone: " 01.02.03.04.05 ",
+    siteLink: " https://planetpizza.netlify.app ",
+    companyName: " Planet Pizza ",
+    ownerName: " Mathieu Gueguin ",
+    email: " mathieugueguin@live.fr ",
+    phone: " 0262477861 ",
+    address: " 94 rue saint-marie, st Denis",
+    hostName: " Hostinger ",
+    hostAddress: " 143 Rue Emile Julien, 34070 Montpellier, France",
+    hostPhone: " 08 92 97 70 93 ",
   };
 
   return (
